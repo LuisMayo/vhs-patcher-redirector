248758619d49bbe046938c66a6f098e07fb67d9c
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2023-10-17 18:33:26 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use steamlocate::SteamDir;
+
+fn unable_locate_dir() {
+    print!("Couldn't locate Steam or Vhs install dir");
+}
+
 fn main() {
     println!("Hello, world!");
+    match SteamDir::locate() {
+        Some(mut steamdir) => match steamdir.app(&611360) {
+            Some(app) => println!("{:#?}", app),
+            None => unable_locate_dir(),
+        },
+        None => unable_locate_dir(),
+    }
 }
