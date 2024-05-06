5aae0ad766ed4ef5aaa4fc9e3d6bffe6de4bd4fe
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2024-05-06 21:09:14 +0200

    wipppp

--- src-tauri/src/clean_up.rs
+++ src-tauri/src/clean_up.rs
@@ -1,3 +1,6 @@
+#[cfg(target_os = "windows")]
+use std::process::Command;
+
 pub fn clean_old_methods() -> Result<String, String> {
     match internal_clean() {
         Ok(msg) => Ok(msg),
@@ -6,12 +9,25 @@ pub fn clean_old_methods() -> Result<String, String> {
 }
 
 #[cfg(target_os = "windows")]
-fn internal_clean() -> Result<(), Box<dyn std::error::Error>> {
-    Command::new("certutil")
-        .args(["-delstore", "Root", "454e1e3b62e6c326"])
-        .spawn()?
-        .wait()?;
-    Ok("Windows detected, removed all certificate (if existing)")
+fn internal_clean() -> Result<String, Box<dyn std::error::Error>> {
+    use std::{io::Read, process::Stdio};
+
+    let command =Command::new("certutil")
+    .args(["-delstore", "Root", "454e1e3b62e6c326"])
+    .stdout(Stdio::piped());
+
+
+    let child = command.spawn()?;
+    let code = child.wait()?;
+    if code.success() {
+        Ok("Windows detected, removed all certificate (if existing)".to_string())
+    } else {
+        if let Some(stdout_bytes) = child.stdout {
+            let output =std::str::from_utf8(stdout_bytes.bytes().into())?;
+            return Err(output)
+        }
+        return Err("");
+    }
 }
 
 #[cfg(not(target_os = "windows"))]
